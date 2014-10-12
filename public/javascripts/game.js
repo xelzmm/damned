5d737d6e5b50800fdd675373d4e36f3ada66697d
xelz <xelzmm@gmail.com>
2014-10-12 14:19:24 +0800

    调整时间格式

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -112,11 +112,29 @@ gameResource.onload = function() {
     document.getElementById('loading').remove();
     init();
 };
+Date.prototype.format = function (format) {
+    var o = {
+        "M+": this.getMonth() + 1, //month
+        "d+": this.getDate(), //day
+        "h+": this.getHours(), //hour
+        "m+": this.getMinutes(), //minute
+        "s+": this.getSeconds(), //second
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
+        "S": this.getMilliseconds() //millisecond
+    };
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)if (new RegExp("(" + k + ")").test(format))
+        format = format.replace(RegExp.$1,
+                RegExp.$1.length == 1 ? o[k] :
+                ("00" + o[k]).substr(("" + o[k]).length));
+    return format;
+};
 var info = function(msg) {
 //    console.log(msg);
     var chatBoard = document.getElementById('chatBoard');
     var autoScroll = chatBoard.scrollTop + chatBoard.clientHeight >= chatBoard.scrollHeight - 5;
-    chatBoard.innerHTML += '[' + new Date().toLocaleTimeString() + '] ' +  msg + '<br />';
+    chatBoard.innerHTML += '[' + new Date().format('hh:mm:ss') + '] ' +  msg + '<br />';
     if(autoScroll) chatBoard.scrollTop = chatBoard.scrollHeight;
 };
 var resize = function() {

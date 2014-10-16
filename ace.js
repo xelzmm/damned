0f808a7a1ae6a4b5eb7842000fdb07268e64ed2c
xelz <xelzmm@gmail.com>
2014-10-16 13:15:54 +0800

    适配阿里云

--- ace.js
+++ ace.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 // some configuration for aliyun ace
+process.env['DEBUG'] = 'damned:*';
 var debug = require('debug');
 debug.useColors = function() {return false};
 debug.log = console.log;
 process.env['PORT'] = 80;
-process.env['DEBUG'] = 'damned:*';
 require('./app');
